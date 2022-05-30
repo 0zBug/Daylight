210d6f0911fc3496f71701eebd87253bda64fdc3
Bug <77031032+0zBug@users.noreply.github.com>
2022-05-30 05:42:43 -0500

    Update wss.py

--- wss.py
+++ wss.py
@@ -1,4 +1,4 @@
-import asyncio, websockets
+import asyncio, websockets, os
 
 discord_socket, client_socket = None, None
 gateway = "wss://gateway.discord.gg/?v=8&encoding=json"
@@ -10,15 +10,26 @@ async def listener(socket: websockets.WebSocketClientProtocol, path):
     if discord_socket != None:
         await socket.close()
         return
-      
+        
     asyncio.get_event_loop().create_task(sender())
     while not discord_socket: await asyncio.sleep(1)
+    if os.name == "nt" or os.name == "dos":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+    print("Connected.")
 
     while True:
         try:
             message = await socket.recv()
             await discord_socket.send(message)
         except Exception as e:
+            if os.name == "nt" or os.name == "dos":
+                os.system("cls")
+            else:
+                os.system("clear")
+            print("Disconnected.")
             await discord_socket.close()
             discord_socket = None
             break
@@ -34,5 +45,6 @@ async def sender():
             except Exception as e:
                 break
 
+print("Disconnected.")
 asyncio.get_event_loop().run_until_complete(websockets.serve(listener, "localhost", 8787))
 asyncio.get_event_loop().run_forever()
